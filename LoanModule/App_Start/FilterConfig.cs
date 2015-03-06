0e9b019e7e64ba7cc6604e04cb770512cc7af7ca
Susan Walsh <susan@equilibriumdesign.com>
2015-03-06 12:36:47 -0800

    Added SSL

--- LoanModule/App_Start/FilterConfig.cs
+++ LoanModule/App_Start/FilterConfig.cs
@@ -7,6 +7,8 @@ namespace LoanModule
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new System.Web.Mvc.AuthorizeAttribute()); //this is the global authorization attribute
+            filters.Add(new RequireHttpsAttribute()); //Require https
         }
     }
 }
